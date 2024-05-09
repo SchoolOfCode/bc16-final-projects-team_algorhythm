21964243f3b50cc7a308e6e88248d30db7661119
Callum Basnett <callumbasnett@gmail.com>
2024-05-09 09:31:57 +0100

    installed TailwindCSS Animated plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from 'daisyui'
+import daisyui from "daisyui";
+import tailwindcssAnimated from "tailwindcss-animated";
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,6 +8,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+// note: 'screens' is used to define the screen size breakpoints, similar to how media queries work in default CSS
+      screens : {
+        'xs' : '320px',
+        'sm' : '640px',
+        'md' : '768px',
+        'lg' : '1024px',
+        'xl' : '1280px',
+        '2xl' : '1536px',
+      },
       colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
@@ -17,5 +27,7 @@ module.exports = {
       },
     },
   },
-  plugins: [daisyui],
+// note: daisyui adds a lot of pre-built styles for components
+// note: tailwindcssAnimated is a tailwind plug that adds a lot of pre-built animations and makes animation waaaaaaay easier
+  plugins: [daisyui, tailwindcssAnimated],
 };

579bb61ad7bdb8642ee0810caa5f0d01c491e3a6
Jack White <jackwhite230@gmail.com>
2024-05-09 14:27:23 +0100

    added id column

--- app/quizcreation/page.tsx
+++ app/quizcreation/page.tsx
@@ -1,81 +1,96 @@
 "use client";
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field } from "formik";
 import { createClient } from "@supabase/supabase-js";
-import * as yup from 'yup';
-import { useState } from 'react'; 
+import * as yup from "yup";
+import { useState } from "react";
 
 const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  );
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
 
 export default function QuizCreation() {
-    const [feedback, setFeedback] = useState(''); 
+  const [feedback, setFeedback] = useState("");
 
-    const QuizSchema = yup.object().shape({
-        question: yup.string().max(120, "Too long!").required("Required."),
-        answer: yup.string().max(100, "Too long!").required("Required."),
-    });
+  const QuizSchema = yup.object().shape({
+    id: yup.string().max(120, "Too long!").required("Required."),
+    question: yup.string().max(120, "Too long!").required("Required."),
+    answer: yup.string().max(100, "Too long!").required("Required."),
+  });
 
-    const QuizValues = {
-        question: "",
-        answer: "",
-    };
+  const QuizValues = {
+    id: "",
+    question: "",
+    answer: "",
+  };
 
-    async function handleSubmit(values, { resetForm }) {
-        const { question, answer } = values;
-        try {
-            const { error } = await supabase.from("contacts").insert({ question, answer });
-            if (error) {
-                throw error;
-            }
-            setFeedback("Form submitted successfully");
-            resetForm();
-        } catch (error) {
-            console.log("Error occurred", { error });
-            setFeedback("An error occurred");
-        }
+  async function handleSubmit(values, { resetForm }) {
+    const { question, answer } = values;
+    try {
+      const { error } = await supabase
+        .from("quiz_creation")
+        .insert({ id, question, answer });
+      if (error) {
+        throw error;
+      }
+      setFeedback("Form submitted successfully");
+      resetForm();
+    } catch (error) {
+      console.log("Error occurred", { error });
+      setFeedback("An error occurred");
     }
+  }
 
-    return (
-        <Formik 
-            enableReinitialize
-            initialValues={QuizValues}
-            validationSchema={QuizSchema}
-            onSubmit={handleSubmit}
-        >
-    {({ isSubmitting }) => ( 
-                <Form>
-        <Field name="question">
+  return (
+    <Formik
+      enableReinitialize
+      initialValues={QuizValues}
+      validationSchema={QuizSchema}
+      onSubmit={handleSubmit}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Field name="id">
             {({ field, meta }) => (
-                <div className="form-control">
-                    <label mb="1.5" fontSize="sm" htmlFor="question">
-                       Question 
-                    </label>
-                    <input {...field} placeholder="Enter question" id="question" />
-                    {meta.error && meta.touched && <p>{meta.error}</p>}
-                </div>
+              <div className="form-control">
+                <label mb="1.5" fontSize="sm" htmlFor="id">
+                  Question Number
+                </label>
+                <input {...field} placeholder="Enter question number" id="id" />
+                {meta.error && meta.touched && <p>{meta.error}</p>}
+              </div>
             )}
-        </Field>
-        <Field name="answer">
+          </Field>
+          <Field name="question">
             {({ field, meta }) => (
-                <div className="form-control">
-                    <label mb="1.5" fontSize="sm" htmlFor="answer">
-                        Answer
-                    </label>
-                    <input {...field} placeholder="Enter answer" id="answer" />
-                    {meta.error && meta.touched && <p>{meta.error}</p>}
-                </div>
+              <div className="form-control">
+                <label mb="1.5" fontSize="sm" htmlFor="question">
+                  Question
+                </label>
+                <input {...field} placeholder="Enter question" id="question" />
+                {meta.error && meta.touched && <p>{meta.error}</p>}
+              </div>
             )}
-        </Field>
+          </Field>
+          <Field name="answer">
+            {({ field, meta }) => (
+              <div className="form-control">
+                <label mb="1.5" fontSize="sm" htmlFor="answer">
+                  Answer
+                </label>
+                <input {...field} placeholder="Enter answer" id="answer" />
+                {meta.error && meta.touched && <p>{meta.error}</p>}
+              </div>
+            )}
+          </Field>
 
-        <div className="button-container">
-                        <button type="submit" className="submit-button">
-                            {isSubmitting ? "Submitting..." : "Submit"}
-                        </button>
-                    </div>
-                </Form>
-            )}
-        </Formik>
-    )
+          <div className="button-container">
+            <button type="submit" className="submit-button">
+              {isSubmitting ? "Submitting..." : "Submit"}
+            </button>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  );
 }

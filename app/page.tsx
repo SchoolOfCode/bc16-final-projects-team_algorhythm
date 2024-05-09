e2facc10541425f2b8233bd2390b5c771c838d24
Igor Silva <igorsilvauk@gmail.com>
2024-05-09 15:39:01 +0100

    added img

--- app/page.tsx
+++ app/page.tsx
@@ -17,10 +17,8 @@ export default async function Index() {
   
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-         Content
-      </div>     
+    <div className="flex-1 w-full flex gap-20 items-center">
+      Content   
     </div>
   )
 }

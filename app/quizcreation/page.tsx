de9932edfb8e980e3d4f5622bd33cbefcab8a187
Callum Basnett <callumbasnett@gmail.com>
2024-05-11 12:25:20 +0100

    added fields for up to four answers

--- app/quizcreation/page.tsx
+++ app/quizcreation/page.tsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 );
 
 export default function QuizCreation() {
@@ -14,19 +14,29 @@ export default function QuizCreation() {
 
   const QuizSchema = yup.object().shape({
     question: yup.string().max(120, "Too long!").required("Required."),
-    answer: yup.string().max(100, "Too long!").required("Required."),
+    correct_answer: yup.string().max(100, "Too long!").required("Required."),
+    answer2: yup.string().max(100, "Too long!").required("Required."),
+    answer3: yup.string().max(100, "Too long!").required("Required."),
+    answer4: yup.string().max(100, "Too long!").required("Required."),
   });
 
   const QuizValues = {
     question: "",
-    answer: "",
+    correct_answer: "",
+    answer2: "",
+    answer3: "",
+    answer4: "",
   };
 
   async function handleSubmit(values, { resetForm }) {
     try {
-      const { error } = await supabase
-        .from("quiz_creation")
-        .insert({ question: values.question, answer: values.answer });
+      const { error } = await supabase.from("quiz_creation").insert({
+        question: values.question,
+        correct_answer: values.correct_answer,
+        answer2: values.answer2,
+        answer3: values.answer3,
+        answer4: values.answer4,
+      });
       if (error) {
         throw error;
       }
@@ -50,28 +60,55 @@ export default function QuizCreation() {
           <Field name="question">
             {({ field, meta }) => (
               <div className="form-control">
-                <label mb="1.5" fontSize="sm" htmlFor="question">
-                  Question
-                </label>
+                <label>Question</label>
                 <input {...field} placeholder="Enter question" id="question" />
                 {meta.error && meta.touched && <p>{meta.error}</p>}
               </div>
             )}
           </Field>
-          <Field name="answer">
+          <Field name="correct_answer">
             {({ field, meta }) => (
               <div className="form-control">
-                <label mb="1.5" fontSize="sm" htmlFor="answer">
-                  Answer
-                </label>
-                <input {...field} placeholder="Enter answer" id="answer" />
+                <label>Correct Answer</label>
+                <input
+                  {...field}
+                  placeholder="Enter the correct answer"
+                  id="correct_answer"
+                />
+                {meta.error && meta.touched && <p>{meta.error}</p>}
+              </div>
+            )}
+          </Field>
+          <Field name="answer2">
+            {({ field, meta }) => (
+              <div className="form-control">
+                <label>Incorrect Answer</label>
+                <input {...field} placeholder="Enter answer 2" id="answer2" />
+                {meta.error && meta.touched && <p>{meta.error}</p>}
+              </div>
+            )}
+          </Field>
+          <Field name="answer3">
+            {({ field, meta }) => (
+              <div className="form-control">
+                <label>Incorrect Answer</label>
+                <input {...field} placeholder="Enter answer 3" id="answer3" />
+                {meta.error && meta.touched && <p>{meta.error}</p>}
+              </div>
+            )}
+          </Field>
+          <Field name="answer4">
+            {({ field, meta }) => (
+              <div className="form-control">
+                <label>Incorrect Answer</label>
+                <input {...field} placeholder="Enter answer 4" id="answer4" />
                 {meta.error && meta.touched && <p>{meta.error}</p>}
               </div>
             )}
           </Field>
 
           <div className="button-container">
-          {feedback && <p>{feedback}</p>}
+            {feedback && <p>{feedback}</p>}
             <button type="submit" className="submit-button">
               {isSubmitting ? "Submitting..." : "Submit"}
             </button>

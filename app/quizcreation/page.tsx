599b1c39ede76089748c44b874856f7164de6af4
Igor Silva <igorsilvauk@gmail.com>
2024-05-12 10:35:47 +0100

    edited form

--- app/quizcreation/page.tsx
+++ app/quizcreation/page.tsx
@@ -3,6 +3,7 @@ import { Formik, Form, Field } from "formik";
 import { createClient } from "@supabase/supabase-js";
 import * as yup from "yup";
 import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -17,6 +18,10 @@ export default function QuizCreation() {
       .number()
       .max(12, "Week must be 1 to 12")
       .required("Required."),
+    day_number: yup
+      .number()
+      .max(5, "Day must be 1 to 5")
+      .required("Required."),
     question: yup.string().max(120, "Too long!").required("Required."),
     correct_answer: yup.string().max(100, "Too long!").required("Required."),
     incorrect_answer1: yup.string().max(100, "Too long!").required("Required."),
@@ -26,6 +31,7 @@ export default function QuizCreation() {
 
   const QuizValues = {
     week_number: "",
+    day_number: "",
     question: "",
     correct_answer: "",
     incorrect_answer1: "",
@@ -37,6 +43,8 @@ export default function QuizCreation() {
     try {
       const { error } = await supabase.from("quiz1").insert({
         week_number: values.week_number,
+        uuid: uuidv4(),
+        day_number: values.day_number,
         question: values.question,
         correct_answer: values.correct_answer,
         incorrect_answer1: values.incorrect_answer1,
@@ -64,14 +72,15 @@ export default function QuizCreation() {
       >
         {({ isSubmitting }) => (
           <Form className="animate-fade-up flex w-[50%] flex-col justify-center gap-2 text-foreground  bg-loginblue p-6 rounded-2xl my-10">
+            <div className="flex flex-row justify-around">
             <Field className="" name="week_number">
               {({ field, meta }: any) => (
                 <div className="form-control">
                   <label className="font-bold">Week</label>
                   <input
-                    className="bg-white w-[25%] rounded-2xl px-4 py-2 bg-inherit border m-6 dark:text-black"
+                    className="bg-white rounded-2xl px-4 py-2 bg-inherit border mx-6 my-1 dark:text-black"
                     {...field}
-                    placeholder="Enter the week number"
+                    placeholder="1"
                     id="week_number"
                     type="number"
                     min="1"
@@ -85,18 +94,42 @@ export default function QuizCreation() {
                 </div>
               )}
             </Field>
+            <Field className="" name="day_number">
+              {({ field, meta }: any) => (
+                <div className="form-control">
+                  <label className="font-bold">Day</label>
+                  <input
+                    className="bg-white rounded-2xl px-4 py-2 bg-inherit border mx-6 my-1 dark:text-black"
+                    {...field}
+                    placeholder="1"
+                    id="day_number"
+                    type="number"
+                    min="1"
+                    max="5"
+                  />
+                  
+                  {meta.error && meta.touched && (
+                    <p className="p-4 bg-foreground/10 text-foreground text-center rounded-2xl text-pink-300">
+                      {meta.error}
+                    </p>
+                  )}
+               </div>
+              )}
+            </Field>
+            </div>
             <Field name="question">
               {({ field, meta }: any) => (
                 <div className="form-control">
                   <label className="font-bold">Question</label>
                   <textarea
-                    className="bg-white rounded-2xl px-4 py-2 bg-inherit border m-6 dark:text-black"
+                    className="bg-white rounded-2xl px-4 py-2 bg-inherit border mx-6 my-1 dark:text-black"
                     {...field}
                     placeholder="Enter question"
                     id="question"
+                    rows='1'
                   />
                   {meta.error && meta.touched && (
-                    <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center rounded-2xl text-pink-300">
+                    <p className="mt-2 p-4 bg-foreground/10 text-foreground text-center rounded-2xl text-pink-300">
                       {meta.error}
                     </p>
                   )}
@@ -108,13 +141,14 @@ export default function QuizCreation() {
                 <div className="form-control">
                   <label className="font-bold">Correct Answer</label>
                   <textarea
-                    className="bg-white rounded-2xl px-4 py-2 bg-inherit border m-6 dark:text-black"
+                    className="bg-white rounded-2xl px-4 py-2 bg-inherit border mx-6 my-1 dark:text-black"
                     {...field}
                     placeholder="Enter the correct answer"
                     id="correct_answer"
+                    rows='1'
                   />
                   {meta.error && meta.touched && (
-                    <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center rounded-2xl text-pink-300">
+                    <p className="mt-2 p-4 bg-foreground/10 text-foreground text-center rounded-2xl text-pink-300">
                       {meta.error}
                     </p>
                   )}
@@ -126,13 +160,14 @@ export default function QuizCreation() {
                 <div className="form-control">
                   <label className="font-bold">Incorrect Answer</label>
                   <textarea
-                    className="bg-white rounded-2xl px-4 py-2 bg-inherit border m-6 dark:text-black"
+                    className="bg-white rounded-2xl px-4 py-2 bg-inherit border mx-6 my-1 dark:text-black"
                     {...field}
                     placeholder="Enter incorrect answer 1"
                     id="incorrect_answer1"
+                    rows='1'
                   />
                   {meta.error && meta.touched && (
-                    <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center rounded-2xl text-pink-300">
+                    <p className="mt-2 p-4 bg-foreground/10 text-foreground text-center rounded-2xl text-pink-300">
                       {meta.error}
                     </p>
                   )}
@@ -144,13 +179,14 @@ export default function QuizCreation() {
                 <div className="form-control">
                   <label className="font-bold">Incorrect Answer</label>
                   <textarea
-                    className="bg-white rounded-2xl px-4 py-2 bg-inherit border m-6 dark:text-black"
+                    className="bg-white rounded-2xl px-4 py-2 bg-inherit border mx-6 my-1 dark:text-black"
                     {...field}
                     placeholder="Enter incorrect answer 2"
                     id="incorrect_answer2"
+                    rows='1'
                   />
                   {meta.error && meta.touched && (
-                    <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center rounded-2xl text-pink-300">
+                    <p className="mt-2 p-4 bg-foreground/10 text-foreground text-center rounded-2xl text-pink-300">
                       {meta.error}
                     </p>
                   )}
@@ -162,13 +198,14 @@ export default function QuizCreation() {
                 <div className="form-control">
                   <label className="font-bold">Incorrect Answer</label>
                   <textarea
-                    className="bg-white rounded-2xl px-4 py-2 bg-inherit border m-6 dark:text-black"
+                    className="bg-white rounded-2xl px-4 py-2 bg-inherit border mx-6 my-1 dark:text-black"
                     {...field}
                     placeholder="Enter incorrect answer 3"
                     id="incorrect_answer3"
+                    rows='1'
                   />
                   {meta.error && meta.touched && (
-                    <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center m-6 rounded-2xl text-pink-300">
+                    <p className="mt-2 p-4 bg-foreground/10 text-foreground text-center rounded-2xl text-pink-300">
                       {meta.error}
                     </p>
                   )}
@@ -180,7 +217,7 @@ export default function QuizCreation() {
               {feedback && <p>{feedback}</p>}
               <button
                 type="submit"
-                className="submit-button w-[60%] bg-socskyblue hover:bg-sky-300 hover:text-white rounded-2xl px-4 py-6 text-foreground text-center text-black dark:text-black"
+                className="submit-button w-[20%] bg-socskyblue hover:bg-sky-300 hover:text-white rounded-2xl px-2 py-4 mt-4 text-foreground text-center text-black dark:text-black"
               >
                 {isSubmitting ? "Submitting..." : "Submit"}
               </button>

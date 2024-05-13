62ae17ec768e74b8c08c360a3dbd25af647e9a21
annamariakoutsoras <annamariakoutsoras@Annamarias-MacBook-Air>
2024-05-13 14:10:35 +0100

    made design edits

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function DashBoard() {
     return (
-        <div className="flex flex-col justify-start items-start pt-10 px-10 w-full">
-          <div className=" flex flex-row ">
+        <div className="flex flex-col  pt-10 px-10 w-full">
+          <div className=" flex flex-row justify-between mb-5 ">
             <h1 className="font-black text-4xl pb-5 text-left ">Welcome to your dashboard</h1>
             <Link
               href="#"
@@ -13,13 +13,13 @@ export default function DashBoard() {
               Modify quizzes
             </Link>
             </div>
-            <div>
-            <select className="select select-bordered w-full max-w-xs bg-loginblue text-white " >
+            <div className="flex flex-row justify-between ">
+            <select className="select select-bordered w-1/5 max-w-xs bg-loginblue text-white " >
   <option disabled selected>Select module</option>
   <option>1. Onboarding</option>
   <option>2. Front End</option>
 </select>
-<select className="select select-bordered w-full max-w-xs bg-loginblue text-white " >
+<select className="select select-bordered w-1/5 max-w-xs bg-loginblue text-white " >
   <option disabled selected>Select day</option>
   <option>Monday</option>
   <option>Tuesday</option>
@@ -27,29 +27,32 @@ export default function DashBoard() {
   <option>Thursday</option>
   <option>Friday</option>
 </select>
-<select className="select select-bordered w-full max-w-xs bg-loginblue text-white " >
+<select className="select select-bordered w-3/5 max-w-xs bg-loginblue text-white " >
   <option disabled selected>Select name</option>
   <option>Annamaria Koutsoras</option>
   <option>Jack White</option>
   <option>Stephen Boyce</option>
 </select>
-<p>Bootcamper</p>
-<p>Cohort</p>
-
 </div>
-
-            <div className=" mt-5 radial-progress text-loginblue " style={{"--value":70}} role="progressbar">70%</div>
-            <div className=" mt-5 radial-progress text-black " style={{"--value":70}} role="progressbar">70%</div>
-            <div className=" mt-5 radial-progress text-loginblue " style={{"--value":70}} role="progressbar">70%</div>
-            <div className=" mt-5 radial-progress text-black " style={{"--value":70}} role="progressbar">70%</div>
-            <div>
-            <p>Daily score</p>
-            <p>Weekly average</p>
-            <p>Daily average</p>
-            <p>Weekly average</p>
+<div className="grid grid-rows-[10vh, 30vh, 10vh] ">
+<div className="grid grid-cols-subgrid col-span-7 justify-items-center items-end h-24 mb-2 ">
+<p className="col-start-2 col-span-2 text-2xl font-bold	">Bootcamper</p>
+<p className="col-start-5 col-span-2 text-2xl font-bold	">Cohort</p>
+</div>
+  <div className="grid grid-cols-subgrid col-span-7 justify-items-center ">
+            <div className=" mt-5 radial-progress text-loginblue col-start-2  " style={{"--value":70, "--size": "8rem"}} role="progressbar">70%</div>
+            <div className=" mt-5 radial-progress text-black  col-start-3 " style={{"--value":70, "--size": "8rem"}} role="progressbar">70%</div>
+            <div className=" mt-5 radial-progress text-loginblue col-start-5 " style={{"--value":70, "--size": "8rem"}} role="progressbar">70%</div>
+            <div className=" mt-5 radial-progress text-black col-start-6 " style={{"--value":70, "--size": "8rem"}} role="progressbar">70%</div>
+            </div>
+            <div className="grid grid-cols-subgrid col-span-7 justify-items-center text-lg mt-5 mb-10">
+            <p className="col-start-2 ">Daily score</p>
+            <p className="col-start-3">Weekly average</p>
+            <p className="col-start-5">Daily average</p>
+            <p className="col-start-6">Weekly average</p>
+            </div>
         </div>
         </div>
-       
     );
 }
 

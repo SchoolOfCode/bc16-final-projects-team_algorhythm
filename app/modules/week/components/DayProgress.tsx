7f1e915fe59d9d8cf5ee70ace7e280d7b58ed91e
Igor Silva <igorsilvauk@gmail.com>
2024-05-16 13:07:22 +0100

    logic completed for dayprogress

--- app/modules/week/components/DayProgress.tsx
+++ app/modules/week/components/DayProgress.tsx
@@ -3,138 +3,84 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function DayProgress({ props }: any) {
-  const [day1, setDay1] = useState(true);
-  const [day2, setDay2] = useState(true);
-  const [day3, setDay3] = useState(true);
-  const [day4, setDay4] = useState(true);
-  const [day5, setDay5] = useState(true);
-  const selected = props.selected;
+  const [day1, setDay1] = useState(true)
+  const [day2, setDay2] = useState(true)
+  const [day3, setDay3] = useState(true)
+  const [day4, setDay4] = useState(true)
+  const [day5, setDay5] = useState(true) 
+  const selected = props.selected
+  const completed = (props.results.data).length !== 0 ? props.results.data : false
 
-  useEffect(() => {
-    const completed =
-      props.results.data.length !== 0
-        ? props.results.data
-            .filter((item: any) => item.day_number === selected)
-            .filter((item: any) => item.success === true)
-        : false;
-    //console.log(day1)
-  }, [selected]);
+  useEffect(()=>{
+    setDay1(completed.some((item: any) => item.day_number === 1 && item.success === true) ? true : false)
+    setDay2(completed.some((item: any) => item.day_number === 2 && item.success === true) ? true : false)
+    setDay3(completed.some((item: any) => item.day_number === 3 && item.success === true) ? true : false)
+    setDay4(completed.some((item: any) => item.day_number === 4 && item.success === true) ? true : false)
+    setDay5(completed.some((item: any) => item.day_number === 5 && item.success === true) ? true : false)
+  }, [props.loaded])
+
+  console.log(day1, day2, day3, day4, day5)
 
   return (
     <ul className="steps steps-vertical w-64 pt-2 animate-fade-right">
       <li className="step h-24">
-        {selected ? (
-          <p
-            className={
-              selected === 1
-                ? "btn btn-ghost bg-loginblue"
-                : "btn btn-ghost bg-gray-300 pointer-events-none"
-            }
-            onClick={
-              selected === 1 ? () => props.setSelected(false) : undefined
-            }
-          >
-            Day 1
-          </p>
+        {selected || day1 ? (<p className={selected === 1 ? "btn btn-ghost bg-loginblue" : "btn btn-ghost bg-gray-300 pointer-events-none"}
+        onClick={selected === 1 ? ()=>props.setSelected(false) : undefined}
+        >Day 1
+        </p>
         ) : (
-          <p
-            className="btn btn-ghost bg-socskyblue hover:bg-loginblue"
-            onClick={() => props.setSelected(1)}
-          >
-            Day 1
-          </p>
+        <p className="btn btn-ghost bg-socskyblue hover:bg-loginblue"
+        onClick={()=>props.setSelected(1)}
+        >Day 1
+        </p>
         )}
       </li>
       <li className="step h-24">
-        {selected ? (
-          <p
-            className={
-              selected === 2
-                ? "btn btn-ghost bg-loginblue"
-                : "btn btn-ghost bg-gray-300 pointer-events-none"
-            }
-            onClick={
-              selected === 2 ? () => props.setSelected(false) : undefined
-            }
-          >
-            Day 2
-          </p>
+        {selected || day2 ? (<p className={selected === 2 ? "btn btn-ghost bg-loginblue" : "btn btn-ghost bg-gray-300 pointer-events-none"}
+        onClick={selected === 2 ? ()=>props.setSelected(false) : undefined}
+        >Day 2
+        </p>
         ) : (
-          <p
-            className="btn btn-ghost bg-socskyblue hover:bg-loginblue"
-            onClick={() => props.setSelected(2)}
-          >
-            Day 2
-          </p>
+        <p className="btn btn-ghost bg-socskyblue hover:bg-loginblue"
+        onClick={()=>props.setSelected(2)}
+        >Day 2
+        </p>
         )}
       </li>
       <li className="step h-24">
-        {selected ? (
-          <p
-            className={
-              selected === 3
-                ? "btn btn-ghost bg-loginblue"
-                : "btn btn-ghost bg-gray-300 pointer-events-none"
-            }
-            onClick={
-              selected === 3 ? () => props.setSelected(false) : undefined
-            }
-          >
-            Day 3
-          </p>
+        {selected || day3 ? (<p className={selected === 3 ? "btn btn-ghost bg-loginblue" : "btn btn-ghost bg-gray-300 pointer-events-none"}
+        onClick={selected === 3 ? ()=>props.setSelected(false) : undefined}
+        >Day 3
+        </p>
         ) : (
-          <p
-            className="btn btn-ghost bg-socskyblue hover:bg-loginblue"
-            onClick={() => props.setSelected(3)}
-          >
-            Day 3
-          </p>
+        <p className="btn btn-ghost bg-socskyblue hover:bg-loginblue"
+        onClick={()=>props.setSelected(3)}
+        >Day 3
+        </p>
         )}
       </li>
       <li className="step h-24">
-        {selected ? (
-          <p
-            className={
-              selected === 4
-                ? "btn btn-ghost bg-loginblue"
-                : "btn btn-ghost bg-gray-300 pointer-events-none"
-            }
-            onClick={
-              selected === 4 ? () => props.setSelected(false) : undefined
-            }
-          >
-            Day 4
-          </p>
+        {selected || day4 ? (<p className={selected === 4 ? "btn btn-ghost bg-loginblue" : "btn btn-ghost bg-gray-300 pointer-events-none"}
+        onClick={selected === 4 ? ()=>props.setSelected(false) : undefined}
+        >Day 4
+        </p>
         ) : (
-          <p
-            className="btn btn-ghost bg-socskyblue hover:bg-loginblue"
-            onClick={() => props.setSelected(4)}
-          >
-            Day 4
-          </p>
+        <p className="btn btn-ghost bg-socskyblue hover:bg-loginblue"
+        onClick={()=>props.setSelected(4)}
+        >Day 4
+        </p>
         )}
       </li>
       <li className="step h-24">
-        {selected ? (
-          <p
-            className={
-              selected === 5
-                ? "btn btn-ghost bg-loginblue"
-                : "btn btn-ghost bg-gray-300 pointer-events-none"
-            }
-            onClick={
-              selected === 5 ? () => props.setSelected(false) : undefined
-            }
-          >
-            Day 5
-          </p>
+        {selected || day5 ? (<p className={selected === 5 ? "btn btn-ghost bg-loginblue" : "btn btn-ghost bg-gray-300 pointer-events-none"}
+        onClick={selected === 5 ? ()=>props.setSelected(false) : undefined}
+        >Day 5
+        </p>
         ) : (
-          <p
-            className="btn btn-ghost bg-socskyblue hover:bg-loginblue"
-            onClick={() => props.setSelected(5)}
-          >
-            Day 5
-          </p>
+        <p className="btn btn-ghost bg-socskyblue hover:bg-loginblue"
+        onClick={()=>props.setSelected(5)}
+        >Day 5
+        </p>
         )}
       </li>
     </ul>
--- app/modules/week/components/Week.tsx
+++ app/modules/week/components/Week.tsx
@@ -5,16 +5,15 @@ import DayProgress from "./DayProgress"
 import Quiz from "./Quiz"
 
 export default function Week({ props }: any){
+    const [loaded, setLoaded] = useState(false)
     const [selected, setSelected] = useState(false)
     const [radialProgress, setRadialProgress] = useState(0)
     props.setRadialProgress = setRadialProgress
     props.radialProgress = radialProgress
     props.setSelected = setSelected
     props.selected = selected
-
-    useEffect(()=>{
-        //console.log(selected)
-    },[selected])
+    props.loaded = loaded
+    props.setLoaded = setLoaded
 
     return (
         <>

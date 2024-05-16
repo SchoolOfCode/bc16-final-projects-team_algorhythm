3cbbc5628cec9748c61084f8bca7dd91bb2e1334
LGenSOC <149721085+LGenSOC@users.noreply.github.com>
2024-05-16 11:57:27 +0200

    footer created

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,7 @@ import { createClient } from "@/utils/supabase/server";
 import Link from "next/link";
 import Image from "next/image";
 
+
 export default async function Footer() {
   const supabase = createClient();
 
@@ -10,13 +11,25 @@ export default async function Footer() {
   } = await supabase.auth.getUser();
 
   return user ? (
-    <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-      <Link href="/">
-        <p>
-          Powered by
-          <Image src="/algologo.png" alt="SoC Logo" width={200} height={200} />
-        </p>
-      </Link>
-    </footer>
-  ) : null;
+  <footer className="w-full border-t border-t-foreground/10 p-8 flex flex-col items-center text-center text-xs">
+    <div className="flex justify-between w-full mb-4">
+      <a href="https://www.schoolofcode.co.uk/about/" className="mx-2">Terms of Use</a>
+      <a href="https://www.schoolofcode.co.uk/privacy/" className="mx-2">Privacy Policy</a>
+      <a href="https://www.schoolofcode.co.uk/carbon-reduction-plan/" className="mx-2">Carbon Reduction Plan</a>
+    </div>
+    <div className="flex items-center justify-center">
+      <p className="mr-4">
+        © Copyright 2024 School of Code. All Rights Reserved.
+        <br />
+        School of Code Ltd is registered in England, Company No. 09793790
+        <br />
+        School of Code, Custard Factory, Gibb Street, Birmingham, B9 4AA
+      </p>
+      <div>
+        <Image src="/algologo.png" alt="SoC Logo" width={200} height={200} />
+        <Image src="/algologo.png" alt="SoC Logo" width={200} height={200} />
+      </div>
+    </div>
+  </footer>
+) : null;
 }

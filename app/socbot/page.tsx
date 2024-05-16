45088309097863bf24505322d09568eba246e97f
Callum Basnett <callumbasnett@gmail.com>
2024-05-16 20:59:57 +0100

    started adding team 10 socbot as iframe

---
+++ app/socbot/page.tsx
@@ -0,0 +1,14 @@
+import react from "react";
+
+export default function SoCBot() {
+  return (
+    <div>
+      <h1>SoCBot</h1>
+      <iframe
+        src="https://soc-llm.vercel.app"
+        className="w-[80%] h-[80vh] border-none"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}

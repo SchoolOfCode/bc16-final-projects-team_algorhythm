b762d641cfa69f28227e4e225abfa898a7efb088
Callum Basnett <callumbasnett@gmail.com>
2024-05-16 22:13:59 +0100

    Added placeholder image for SoCBot and adjusted sizing of iframe

--- app/socbot/page.tsx
+++ app/socbot/page.tsx
@@ -1,14 +1,24 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function SoCBot() {
   return (
-    <div>
-      <h1>SoCBot</h1>
-      <div className="overflow-hidden rounded-3xl">
+    <div className="flex flex-col items-center">
+      <Link href="https://soc-llm.vercel.app">
+        <Image
+          src="/botplaceholder.png"
+          alt="SoCBot"
+          width={200}
+          height={200}
+          className="align-middle justify-center flex flex-auto mb-4 mt-4"
+        />
+      </Link>
+      <div className="overflow-hidden rounded-3xl m-4">
         <iframe
           src="https://soc-llm.vercel.app"
-          width="720"
-          height="1080"
+          width="1440"
+          height="1024"
           allowFullScreen
           className="-mt-24 -mb-12" // Add this line to apply negative top margin
         />

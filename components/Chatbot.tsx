ce311d06882816ccd80d21967895c04c58b4b7c6
Igor Silva <igorsilvauk@gmail.com>
2024-05-17 08:44:37 +0100

    fixed path`s key names,  and styled  a bit

--- components/Chatbot.tsx
+++ components/Chatbot.tsx
@@ -1,33 +1,33 @@
 "use client";
 import React, { useState } from "react";
-import Image from "next/image";
-import Link from "next/link";
 
 export default function Chatbot() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-      <button
-        className="btn fixed bottom-10 right-20 z-50"
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        SoCBot
+    <div className="chat chat-end">
+      <div className="chat-bubble fixed bottom-10 right-14 z-50 shadow-xl bg-loginblue text-white hover:bg-sky-300 hover:text-black flex items-center py-2 hover:cursor-pointer"  
+      onClick={() => setIsOpen(!isOpen)}>
+        
+        <p className="pr-2">SoCBot</p>
+      
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
           className="w-10 h-10"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M8.625 9.75a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 0 1 .778-.332 48.294 48.294 0 0 0 5.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
           />
         </svg>
-      </button>
+        </div>
+      </div>
       <div
         className={`fixed -bottom-10 right-20 z-50 drop-shadow transition-transform duration-500 ease-in-out transform ${
           isOpen ? "translate-y-0" : "translate-y-full"
@@ -38,7 +38,7 @@ export default function Chatbot() {
         }}
       >
         <button
-          className="btn btn-circle fixed -right-4 -top-5"
+          className="btn btn-circle fixed -right-4 -top-5 bg-socskyblue border-1 border-black"
           onClick={() => setIsOpen(!isOpen)}
         >
           <svg

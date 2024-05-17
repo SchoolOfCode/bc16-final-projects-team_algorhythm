89d513cb1d52c57391edc2d5d621d06430ded51e
LGenSOC <149721085+LGenSOC@users.noreply.github.com>
2024-05-17 12:56:17 +0200

    added links to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -34,50 +34,50 @@ export default async function Footer() {
   <footer className="footer p-10 bg-base-300 text-base-content">
       <nav>
         <h6 className="footer-title">Help and Support</h6> 
+        <a href="https://learn.schoolofcode.co.uk/"className="link link-hover">LearnWorlds</a>     
         <a className="link link-hover">Documentation</a>
-        <a className="link link-hover">Cookie Policy</a>
         <a className="link link-hover">FAQs</a>
-        <a className="link link-hover">Contact us</a>
+        <a href="mailto:info@schoolofcode.co.uk"className="link link-hover">Contact us</a>     
       </nav> 
        <nav>
         <h6 className="footer-title">Resources</h6> 
-        <a className="link link-hover">CodeWars</a>
-        <a className="link link-hover">Fiddle</a>
-        <a className="link link-hover">Exercism</a>
-        <a className="link link-hover">Free Code Camp</a>
+        <a href="https://www.codewars.com/"className="link link-hover">CodeWars</a>
+        <a href="https://jsfiddle.net/"className="link link-hover">JSFiddle</a>
+        <a href="https://exercism.org/"className="link link-hover">Exercism</a>
+        <a href="https://freecodecamp.org/"className="link link-hover">Free Code Camp</a>
       </nav> 
       <nav>
         <h6 className="footer-title">Company</h6> 
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Terms of Use</a>
-        <a className="link link-hover">Privacy Policy</a>
-        <a className="link link-hover">Carbon Reduction Plan</a>
+        <a href="https://www.schoolofcode.co.uk/"className="link link-hover">About us</a>
+        <a href="https://learn.schoolofcode.co.uk/"className="link link-hover">Terms of Use</a>
+        <a href="https://www.schoolofcode.co.uk/privacy/"className="link link-hover">Privacy and Cookie Policy</a>
+        <a href="https://www.schoolofcode.co.uk/carbon-reduction-plan/"className="link link-hover">Carbon Reduction Plan</a>
  <div className="w-full mt-4 flex flex-col items-center">
         <Image src="/algologo.png" alt="SoC Logo" width={100} height={100} />
       </div>
-         <p className="mt-4 text-center text-[8px]">
+         <p className="mt-4 text-center text-[10px]">
           © Copyright 2024 School of Code. All Rights Reserved. School of Code Ltd is registered in England, Company No. 09793790. School of Code, Custard Factory, Gibb Street, Birmingham, B9 4AA
         </p>
       </nav> 
       <nav>
         <h6 className="footer-title">Social</h6>
 <div className="grid grid-flow-col gap-4">
-  <a>
+  <a href="https://twitter.com/theSchoolOfCode/" target="_blank" rel="noopener noreferrer">
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current">
       <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
     </svg>
   </a>
-  <a>
+  <a href="https://www.youtube.com/channel/UCKBzheEKcrqsaJhMV0f_Dmg" target="_blank" rel="noopener noreferrer">
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current">
       <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
     </svg>
   </a>
-  <a>
+ <a href="https://www.facebook.com/schoolofcode/" target="_blank" rel="noopener noreferrer">
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current">
       <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
     </svg>
   </a>
-  <a>
+ <a href="https://www.linkedin.com/school/school-of-code/" target="_blank" rel="noopener noreferrer">
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current">
       <path d="M19 0h-14c-2.75 0-5 2.25-5 5v14c0 2.75 2.25 5 5 5h14c2.75 0 5-2.25 5-5v-14c0-2.75-2.25-5-5-5zm-11 19h-3v-10h3v10zm-1.5-11.14c-.97 0-1.75-.78-1.75-1.75s.78-1.75 1.75-1.75 1.75.78 1.75 1.75-.78 1.75-1.75 1.75zm13.5 11.14h-3v-5.5c0-1.34-.04-3.06-1.87-3.06-1.87 0-2.16 1.46-2.16 2.97v5.59h-3v-10h2.88v1.36h.04c.4-.76 1.36-1.56 2.79-1.56 2.99 0 3.54 1.97 3.54 4.52v5.68z"></path>
     </svg>

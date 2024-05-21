36880c777ec24e5a59e4f32de8290214d3137bf9
Igor Silva <igorsilvauk@gmail.com>
2024-05-21 14:31:09 +0100

    fetched table weeks

--- app/dashboard/admin/EditQuizzes.tsx
+++ app/dashboard/admin/EditQuizzes.tsx
@@ -1,9 +1,9 @@
-import { createClient } from '@supabase/supabase-js';
-import { useState, useEffect } from 'react';
+import { createClient } from "@supabase/supabase-js";
+import { useState, useEffect } from "react";
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
 interface Question {
@@ -33,9 +33,12 @@ interface EditingQuestion {
   question: Question;
 }
 
-export default function EditQuizzes(): JSX.Element {
+export default function EditQuizzes({ weeksNames }: any): JSX.Element {
   const [weeksData, setWeeksData] = useState<Week[]>([]);
-  const [editingQuestion, setEditingQuestion] = useState<EditingQuestion | null>(null);
+  const [editingQuestion, setEditingQuestion] =
+    useState<EditingQuestion | null>(null);
+
+  console.log(weeksNames.data);
 
   const handleEditQuestion = (
     uuid: string,
@@ -44,32 +47,45 @@ export default function EditQuizzes(): JSX.Element {
     questionIndex: number,
     question: Question
   ) => {
-    setEditingQuestion({ uuid, week_number, day_number, questionIndex, question });
+    setEditingQuestion({
+      uuid,
+      week_number,
+      day_number,
+      questionIndex,
+      question,
+    });
   };
 
   const handleSaveQuestion = async (updatedQuestion: EditingQuestion) => {
     const { uuid, question } = updatedQuestion;
-    const { error } = await supabase.from('quizzes').update(question).match({ uuid });
+    const { error } = await supabase
+      .from("quizzes")
+      .update(question)
+      .match({ uuid });
 
     if (!error) {
       setWeeksData((prevWeeksData) =>
-        prevWeeksData.map((week) =>
-          week.week_number === updatedQuestion.week_number
-            ? {
-                ...week,
-                days: week.days.map((day) =>
-                  day.day_number === updatedQuestion.day_number
-                    ? {
-                        ...day,
-                        questions: day.questions.map((q, index) =>
-                          index === updatedQuestion.questionIndex ? updatedQuestion.question : q
-                        ),
-                      }
-                    : day
-                ),
-              }
-            : week
-        ).sort((a, b) => b.week_number - a.week_number)
+        prevWeeksData
+          .map((week) =>
+            week.week_number === updatedQuestion.week_number
+              ? {
+                  ...week,
+                  days: week.days.map((day) =>
+                    day.day_number === updatedQuestion.day_number
+                      ? {
+                          ...day,
+                          questions: day.questions.map((q, index) =>
+                            index === updatedQuestion.questionIndex
+                              ? updatedQuestion.question
+                              : q
+                          ),
+                        }
+                      : day
+                  ),
+                }
+              : week
+          )
+          .sort((a, b) => b.week_number - a.week_number)
       );
 
       setEditingQuestion(null);
@@ -78,139 +94,202 @@ export default function EditQuizzes(): JSX.Element {
 
   useEffect(() => {
     const fetchData = async () => {
-      const { data: quizzesData, error: quizzesError } = await supabase.from('quizzes').select('*');
-      const { data: weeksData, error: weeksError } = await supabase.from('weeks').select('*');
-  
+      const { data: quizzesData, error: quizzesError } = await supabase
+        .from("quizzes")
+        .select("*");
+      const { data: weeksData, error: weeksError } = await supabase
+        .from("weeks")
+        .select("*");
+
       if (quizzesError || weeksError) {
-        console.error('Error fetching data:', quizzesError || weeksError);
+        console.error("Error fetching data:", quizzesError || weeksError);
         return;
       }
-  
+
       if (quizzesData && weeksData) {
         console.log(weeksData);
         const groupedByWeek = quizzesData.reduce<Week[]>((acc, curr) => {
-          const weekIndex = acc.findIndex((w) => w.week_number === curr.week_number);
-          const weekTitle = weeksData.find((w) => w.week_number === curr.week_number)?.title;
-  
+          const weekIndex = acc.findIndex(
+            (w) => w.week_number === curr.week_number
+          );
+          const weekTitle = weeksData.find(
+            (w) => w.week_number === curr.week_number
+          )?.title;
+
           if (weekIndex !== -1) {
-            const dayIndex = acc[weekIndex].days.findIndex((d) => d.day_number === curr.day_number);
+            const dayIndex = acc[weekIndex].days.findIndex(
+              (d) => d.day_number === curr.day_number
+            );
             if (dayIndex !== -1) {
               acc[weekIndex].days[dayIndex].questions.push(curr);
             } else {
-              acc[weekIndex].days.push({ day_number: curr.day_number, questions: [curr] });
+              acc[weekIndex].days.push({
+                day_number: curr.day_number,
+                questions: [curr],
+              });
             }
           } else {
-            acc.push({ week_number: curr.week_number, title: weekTitle, days: [{ day_number: curr.day_number, questions: [curr] }] });
+            acc.push({
+              week_number: curr.week_number,
+              title: weekTitle,
+              days: [{ day_number: curr.day_number, questions: [curr] }],
+            });
           }
           return acc;
         }, []);
-  
-        const sortedWeeksData = groupedByWeek.sort((a, b) => b.week_number - a.week_number);
+
+        const sortedWeeksData = groupedByWeek.sort(
+          (a, b) => b.week_number - a.week_number
+        );
         console.log(sortedWeeksData);
         setWeeksData(sortedWeeksData);
       }
     };
-  
+
     fetchData();
   }, []);
-  
 
   return (
     <div className="w-2/3">
-      {weeksData.slice().reverse().map((week) => (
-  <div key={week.week_number} className="collapse collapse-arrow bg-socskyblue">
-    <input type="checkbox" />
-    <div className="collapse-title text-xl font-medium">
-      Week {week.week_number}: {week.title}
-    </div>
-          <div className="collapse-content">
-            {week.days.map((day) => (
-              <div key={day.day_number} className="collapse bg-base-200">
-                <input type="checkbox" />
-                <div className="collapse-title text-xl font-medium">
-                  Day {day.day_number}
+      {weeksData
+        .slice()
+        .reverse()
+        .map((week) => (
+          <div
+            key={week.week_number}
+            className="collapse collapse-arrow bg-socskyblue"
+          >
+            <input type="checkbox" />
+            <div className="collapse-title text-xl font-medium">
+              Week {week.week_number}: {week.title}
+            </div>
+            <div className="collapse-content">
+              {week.days.map((day) => (
+                <div key={day.day_number} className="collapse bg-base-200">
+                  <input type="checkbox" />
+                  <div className="collapse-title text-xl font-medium">
+                    Day {day.day_number}
+                  </div>
+                  <div className="collapse-content">
+                    {day.questions.map((question, questionIndex) => (
+                      <div key={questionIndex}>
+                        {editingQuestion?.week_number === week.week_number &&
+                        editingQuestion?.day_number === day.day_number &&
+                        editingQuestion?.questionIndex === questionIndex ? (
+                          <div>
+                            <label>Question:</label>
+                            <input
+                              value={editingQuestion.question.question}
+                              onChange={(e) =>
+                                setEditingQuestion((prevState) => ({
+                                  ...prevState!,
+                                  question: {
+                                    ...prevState!.question,
+                                    question: e.target.value,
+                                  },
+                                }))
+                              }
+                            />
+                            <label>Correct Answer:</label>
+                            <input
+                              value={editingQuestion.question.correct_answer}
+                              onChange={(e) =>
+                                setEditingQuestion((prevState) => ({
+                                  ...prevState!,
+                                  question: {
+                                    ...prevState!.question,
+                                    correct_answer: e.target.value,
+                                  },
+                                }))
+                              }
+                            />
+                            <label>Incorrect Answer 1:</label>
+                            <input
+                              value={
+                                editingQuestion.question.incorrect_answer1 || ""
+                              }
+                              onChange={(e) =>
+                                setEditingQuestion((prevState) => ({
+                                  ...prevState!,
+                                  question: {
+                                    ...prevState!.question,
+                                    incorrect_answer1: e.target.value,
+                                  },
+                                }))
+                              }
+                            />
+                            <label>Incorrect Answer 2:</label>
+                            <input
+                              value={
+                                editingQuestion.question.incorrect_answer2 || ""
+                              }
+                              onChange={(e) =>
+                                setEditingQuestion((prevState) => ({
+                                  ...prevState!,
+                                  question: {
+                                    ...prevState!.question,
+                                    incorrect_answer2: e.target.value,
+                                  },
+                                }))
+                              }
+                            />
+                            <label>Incorrect Answer 3:</label>
+                            <input
+                              value={
+                                editingQuestion.question.incorrect_answer3 || ""
+                              }
+                              onChange={(e) =>
+                                setEditingQuestion((prevState) => ({
+                                  ...prevState!,
+                                  question: {
+                                    ...prevState!.question,
+                                    incorrect_answer3: e.target.value,
+                                  },
+                                }))
+                              }
+                            />
+                            <button
+                              onClick={() =>
+                                handleSaveQuestion(editingQuestion)
+                              }
+                            >
+                              Save
+                            </button>
+                          </div>
+                        ) : (
+                          <div>
+                            <h3>Question {questionIndex + 1}</h3>
+                            <p>{question.question}</p>
+                            <p>Correct Answer: {question.correct_answer}</p>
+                            <p>Incorrect Answers:</p>
+                            <ul>
+                              <li>{question.incorrect_answer1}</li>
+                              <li>{question.incorrect_answer2}</li>
+                              <li>{question.incorrect_answer3}</li>
+                            </ul>
+                            <button
+                              onClick={() =>
+                                handleEditQuestion(
+                                  question.uuid,
+                                  week.week_number,
+                                  day.day_number,
+                                  questionIndex,
+                                  question
+                                )
+                              }
+                            >
+                              Edit
+                            </button>
+                          </div>
+                        )}
+                      </div>
+                    ))}
+                  </div>
                 </div>
-                <div className="collapse-content">
-                  {day.questions.map((question, questionIndex) => (
-                    <div key={questionIndex}>
-                      {editingQuestion?.week_number === week.week_number &&
-                      editingQuestion?.day_number === day.day_number &&
-                      editingQuestion?.questionIndex === questionIndex ? (
-                        <div>
-                          <label>Question:</label>
-                          <input
-                            value={editingQuestion.question.question}
-                            onChange={(e) =>
-                              setEditingQuestion((prevState) => ({
-                                ...prevState!,
-                                question: { ...prevState!.question, question: e.target.value },
-                              }))
-                            }
-                          />
-                          <label>Correct Answer:</label>
-                          <input
-                            value={editingQuestion.question.correct_answer}
-                            onChange={(e) =>
-                              setEditingQuestion((prevState) => ({
-                                ...prevState!,
-                                question: { ...prevState!.question, correct_answer: e.target.value },
-                              }))
-                            }
-                          />
-                          <label>Incorrect Answer 1:</label>
-                          <input
-                            value={editingQuestion.question.incorrect_answer1 || ''}
-                            onChange={(e) =>
-                              setEditingQuestion((prevState) => ({
-                                ...prevState!,
-                                question: { ...prevState!.question, incorrect_answer1: e.target.value },
-                              }))
-                            }
-                          />
-                          <label>Incorrect Answer 2:</label>
-                          <input
-                            value={editingQuestion.question.incorrect_answer2 || ''}
-                            onChange={(e) =>
-                              setEditingQuestion((prevState) => ({
-                                ...prevState!,
-                                question: { ...prevState!.question, incorrect_answer2: e.target.value },
-                              }))
-                            }
-                          />
-                          <label>Incorrect Answer 3:</label>
-                          <input
-                            value={editingQuestion.question.incorrect_answer3 || ''}
-                            onChange={(e) =>
-                              setEditingQuestion((prevState) => ({
-                                ...prevState!,
-                                question: { ...prevState!.question, incorrect_answer3: e.target.value },
-                              }))
-                            }
-                          />
-                          <button onClick={() => handleSaveQuestion(editingQuestion)}>Save</button>
-                        </div>
-                      ) : (
-                        <div>
-                          <h3>Question {questionIndex + 1}</h3>
-                          <p>{question.question}</p>
-                          <p>Correct Answer: {question.correct_answer}</p>
-                          <p>Incorrect Answers:</p>
-                          <ul>
-                            <li>{question.incorrect_answer1}</li>
-                            <li>{question.incorrect_answer2}</li>
-                            <li>{question.incorrect_answer3}</li>
-                          </ul>
-                          <button onClick={() => handleEditQuestion(question.uuid, week.week_number, day.day_number, questionIndex, question)}>Edit</button>
-                        </div>
-                      )}
-                    </div>
-                  ))}
-                </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,7 +6,6 @@ import StudentDashBoard from "./student/StudentDashBoard";
 import AdminDashBoard from "./admin/AdminDashBoard";
 /* imports as required */
 
-
 export default async function DashBoard() {
   const supabase = createClient();
   //Checking if the user is loged in
@@ -20,46 +19,39 @@ export default async function DashBoard() {
   // Check ends
 
   //Checking if the user is Coach or Student
-  const role = await supabase
-    .from('roles')
-    .select("*")
+  const role = await supabase.from("roles").select("*");
 
-  const admin = role.data![0].role === 'admin'
+  const admin = role.data![0].role === "admin";
   // Check ends, now it applies to JSX to render based on role
 
-  const data = await supabase
-    .from("results")
-    .select("*")
+  const data = await supabase.from("results").select("*");
   //console.log(progressBar1.data);
 
   // Gets user name (applied only on student dashboard)
-  const userData = await supabase
-    .from("profiles")
-    .select('*')
+  const userData = await supabase.from("profiles").select("*");
 
   // Storing img url here if it exists
-  let img;  
+  let img;
   // Check if user has image
-  const searchImg = await supabase.storage.from('avatars').list(user!.id + '/', {
-    limit: 1,
-    offset: 0,
-  });
-  if(searchImg?.data!.length){
+  const searchImg = await supabase.storage
+    .from("avatars")
+    .list(user!.id + "/", {
+      limit: 1,
+      offset: 0,
+    });
+  if (searchImg?.data!.length) {
     // Fetch user image
-  const image = supabase
-    .storage
-    .from('avatars')
-    .getPublicUrl(user!.id + '/avatar.png')
-    img = image.data.publicUrl
+    const image = supabase.storage
+      .from("avatars")
+      .getPublicUrl(user!.id + "/avatar.png");
+    img = image.data.publicUrl;
   }
-  
-  
 
-  return (
-    admin ? (
-        <AdminDashBoard data={data}/> 
-    ) : (
-        <StudentDashBoard data={data} userData={userData} img={img}/>
-    )
+  const weeksNames = await supabase.from("weeks").select("*");
+
+  return admin ? (
+    <AdminDashBoard data={data} weeksNames={weeksNames} />
+  ) : (
+    <StudentDashBoard data={data} userData={userData} img={img} />
   );
 }

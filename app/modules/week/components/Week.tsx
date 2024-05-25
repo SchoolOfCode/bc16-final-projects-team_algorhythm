46b80eac557f2fff4329df70b9adc923b92e573a
cbazzy <151874965+cbazzy@users.noreply.github.com>
2024-05-25 18:15:03 +0100

    Changes to week display and quiz display

--- app/modules/week/components/Week.tsx
+++ app/modules/week/components/Week.tsx
@@ -1,89 +1,95 @@
-'use client'
-import React, { useState, useEffect } from "react"
-import Image from "next/image"
-import DayProgress from "./DayProgress"
-import Quiz from "./Quiz"
+"use client";
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import DayProgress from "./DayProgress";
+import Quiz from "./Quiz";
 
-export default function Week({ props }: any){
-    const [day1, setDay1] = useState(true)
-    const [day2, setDay2] = useState(true)
-    const [day3, setDay3] = useState(true)
-    const [day4, setDay4] = useState(true)
-    const [day5, setDay5] = useState(true) 
-    const [refresh, setRefresh] = useState(false)
-    const [selected, setSelected] = useState(false)
-    const [radialProgress, setRadialProgress] = useState(0)
-    props.setRadialProgress = setRadialProgress
-    props.radialProgress = radialProgress
-    props.setSelected = setSelected
-    props.selected = selected
-    props.setRefresh = setRefresh
-    props.refresh = refresh
-    props.day1 = day1
-    props.setDay1 = setDay1
-    props.day2 = day2
-    props.setDay2 = setDay2
-    props.day3 = day3
-    props.setDay3 = setDay3
-    props.day4 = day4
-    props.setDay4 = setDay4
-    props.day5 = day5
-    props.setDay5 = setDay5
+export default function Week({ props }: any) {
+  const [day1, setDay1] = useState(true);
+  const [day2, setDay2] = useState(true);
+  const [day3, setDay3] = useState(true);
+  const [day4, setDay4] = useState(true);
+  const [day5, setDay5] = useState(true);
+  const [refresh, setRefresh] = useState(false);
+  const [selected, setSelected] = useState(false);
+  const [radialProgress, setRadialProgress] = useState(0);
+  props.setRadialProgress = setRadialProgress;
+  props.radialProgress = radialProgress;
+  props.setSelected = setSelected;
+  props.selected = selected;
+  props.setRefresh = setRefresh;
+  props.refresh = refresh;
+  props.day1 = day1;
+  props.setDay1 = setDay1;
+  props.day2 = day2;
+  props.setDay2 = setDay2;
+  props.day3 = day3;
+  props.setDay3 = setDay3;
+  props.day4 = day4;
+  props.setDay4 = setDay4;
+  props.day5 = day5;
+  props.setDay5 = setDay5;
 
-    return (
-        <>
-        <h1 className="text-black font-semibold text-4xl m-5 animate-fade dark:text-white">
-            {props.data[0].title}
-        </h1>
-        <div className="flex items-start w-full justify-evenly animate-fade">
-            <DayProgress props={props}/>
-            {!selected ? (
-                <div className="flex flex-col w-[30%] items-center">
-                  <h1 className="text-black font-semibold text-2xl m-5 dark:text-white">
-                    Time to test your knowledge
-                  </h1>
-                  <Image
-                    src={`/quizicons/${props.data[0].img}.png`}
-                    alt="Content"
-                    width={400}
-                    height={400}
-                    className="w-full max-w-[400px] m-2"
-                   />
-                </div>
-                ) : ( 
-                <form className="flex flex-col w-[30%] items-center">
-                    <Quiz props={props}/>
-                </form>
-                )
-            }
-            {selected && radialProgress ? ( 
-                <div className="w-64 flex flex-col items-center justify-between">
-                    <div className="h-28"/>
-                        <p className="pb-3">Quiz Progress   </p>
-                        <div className="radial-progress text-gray-300 bg-transparent shadow-lg" 
-                            style={{ 
-                                "--value": 100,
-                                "--size": "10rem",
-                                "--thickness": "15px" 
-                            } as React.CSSProperties} // Explicitly type as React.CSSProperties
-                            role="progressbar">
-                            <div className="radial-progress text-loginblue  z-50" 
-                            style={{ 
-                                "--value": radialProgress,
-                                "--size": "10rem",
-                                "--thickness": "15px" 
-                            } as React.CSSProperties} // Explicitly type as React.CSSProperties
-                            role="progressbar">
-                            {radialProgress}%
-                        </div>
-                        </div>
-                    <div className="h-28"/>
-                </div>
-            ) : ( 
-                <div className="w-64"/>
-                
-            )}
-        </div>
-      </>
-    )
+  return (
+    <>
+      <h1 className="text-black font-semibold text-4xl m-5 animate-fade dark:text-white">
+        {props.data[0].title}
+      </h1>
+      <div className="flex items-start w-full justify-evenly animate-fade md:flex-col md:items-center">
+        <DayProgress props={props} />
+        {!selected ? (
+          <div className="flex flex-col w-[30%] items-center md:order-first md:w-[80%]">
+            <h1 className="text-black font-semibold text-2xl m-5 dark:text-white">
+              Time to test your knowledge
+            </h1>
+            <Image
+              src={`/quizicons/${props.data[0].img}.png`}
+              alt="Content"
+              width={400}
+              height={400}
+              className="w-full max-w-[400px] m-2 md:hidden"
+            />
+          </div>
+        ) : (
+          <form className="flex flex-col w-[30%] items-center">
+            <Quiz props={props} />
+          </form>
+        )}
+        {selected && radialProgress ? (
+          <div className="w-64 flex flex-col items-center justify-between">
+            <div className="h-28" />
+            <p className="pb-3">Quiz Progress </p>
+            <div
+              className="radial-progress text-gray-300 bg-transparent shadow-lg"
+              style={
+                {
+                  "--value": 100,
+                  "--size": "10rem",
+                  "--thickness": "15px",
+                } as React.CSSProperties
+              } // Explicitly type as React.CSSProperties
+              role="progressbar"
+            >
+              <div
+                className="radial-progress text-loginblue  z-50"
+                style={
+                  {
+                    "--value": radialProgress,
+                    "--size": "10rem",
+                    "--thickness": "15px",
+                  } as React.CSSProperties
+                } // Explicitly type as React.CSSProperties
+                role="progressbar"
+              >
+                {radialProgress}%
+              </div>
+            </div>
+            <div className="h-28" />
+          </div>
+        ) : (
+          <div className="w-64" />
+        )}
+      </div>
+    </>
+  );
 }
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,12 +1,12 @@
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import { SubmitButton } from "@/components/Submit";
-import {uploadImage, updateProf} from "./update";
+import { uploadImage, updateProf } from "./update";
 
 export default async function Profile({
   searchParams,
 }: {
-  searchParams: { message: string; img: string, imgMessage: string };
+  searchParams: { message: string; img: string; imgMessage: string };
 }) {
   const supabase = createClient();
 
@@ -19,17 +19,18 @@ export default async function Profile({
   }
 
   // Check if there`s any image inside user storage
-  const { data, error } = await supabase.storage.from('avatars').list(user.id + '/', {
-    limit: 1,
-    offset: 0,
-  });
-  if(data?.length){
+  const { data, error } = await supabase.storage
+    .from("avatars")
+    .list(user.id + "/", {
+      limit: 1,
+      offset: 0,
+    });
+  if (data?.length) {
     // Fetch user image
-  const { data } = supabase
-    .storage
-    .from('avatars')
-    .getPublicUrl(user.id + '/avatar.png')
-    searchParams.img = data.publicUrl
+    const { data } = supabase.storage
+      .from("avatars")
+      .getPublicUrl(user.id + "/avatar.png");
+    searchParams.img = data.publicUrl;
   }
 
   // Functions to update first and last name
@@ -39,53 +40,62 @@ export default async function Profile({
     const last_name = formData.get("last_name") as string;
     const data: any = {
       first_name,
-      last_name
-    }
-    const update = await updateProf(data)
+      last_name,
+    };
+    const update = await updateProf(data);
     if (update) {
       return redirect("?message=Profile updated successfully!");
     }
-      return redirect("?message=Could not update profile");
-  }
-  
-  // function to update user image
-  const handleImage = async(formData: FormData)=>{
-    'use server'
-    const image = formData.get("file") as File;
-    const update = await uploadImage(image)
+    return redirect("?message=Could not update profile");
+  };
 
-    if(update){
+  // function to update user image
+  const handleImage = async (formData: FormData) => {
+    "use server";
+    const image = formData.get("file") as File;
+    const update = await uploadImage(image);
+
+    if (update) {
       return redirect("?imgMessage=Image uploaded successfully!");
     }
     return redirect("?imgMessage=Could not upload image");
-  }
+  };
 
   return (
-    <div className="flex-1 w-full flex items-center justify-evenly animate-fade">
+    <div className="flex-1 w-full flex items-center justify-evenly animate-fade md:m-10">
       <div className="flex bg-loginblue rounded-2xl p-10 justify-evenly gap-10">
         <form className="space-y-4 text-center">
-          <label htmlFor="file-upload" className="flex flex-col items-center cursor-pointer">
-            <img 
-            className="bg-white rounded-full w-24 h-24" 
-            alt="User image"
-            width={100}
-            height={100}
-            src={searchParams?.img ? searchParams.img : '/usericon.png'} 
+          <label
+            htmlFor="file-upload"
+            className="flex flex-col items-center cursor-pointer"
+          >
+            <img
+              className="bg-white rounded-full w-24 h-24"
+              alt="User image"
+              width={100}
+              height={100}
+              src={searchParams?.img ? searchParams.img : "/usericon.png"}
             />
-            <input type="file" name="file" id="file-upload" className="file-input bg-socskyblue dark:text-black file-input-bordered file-input-xs w-full max-w-xs mt-5" required/>
+            <input
+              type="file"
+              name="file"
+              id="file-upload"
+              className="file-input bg-socskyblue dark:text-black file-input-bordered file-input-xs w-full max-w-xs mt-5"
+              required
+            />
           </label>
           <SubmitButton
-              formAction={handleImage}
-              className="bg-socskyblue hover:bg-sky-300 hover:text-white rounded-2xl px-5 py-2 text-foreground text-black dark:text-black "
-              pendingText="Saving..."
-            >
-              Save
-            </SubmitButton>
-            {searchParams?.imgMessage && (
-              <p className="mt-4 p-4 bg-foreground/30 text-foreground text-center rounded-2xl text-green-200">
-                {searchParams.imgMessage}
-              </p>
-            )}
+            formAction={handleImage}
+            className="bg-socskyblue hover:bg-sky-300 hover:text-white rounded-2xl px-5 py-2 text-foreground text-black dark:text-black "
+            pendingText="Saving..."
+          >
+            Save
+          </SubmitButton>
+          {searchParams?.imgMessage && (
+            <p className="mt-4 p-4 bg-foreground/30 text-foreground text-center rounded-2xl text-green-200">
+              {searchParams.imgMessage}
+            </p>
+          )}
         </form>
         <div className="flex flex-col">
           <form className="flex flex-col gap-1 px-5">
